654f46cfff696a8795cb48ed80c1f377ee826950
max acosta-h <max acosta-h>
2016-06-04 11:42:45 -0400

    Proposed solution

--- src/Menu.cpp
+++ src/Menu.cpp
@@ -5,100 +5,97 @@
  *      Author: raydelto
  */
 
-#include "Menu.h"
 #include <iostream>
+#include <string>
+#include <vector>
+#include <fstream>
+
 using namespace std;
 
-Menu::Menu()
+void makelist();
+void addtolist();
+void checkitem();
+void displaylist();
+struct list
 {
-
+    int quant;
+    string items;
+    string place;
+    string date;
+};
+int main()
+{                //show to the user an interface to select an option
+    int a;
+    cout << "                        *WELCOME*" << endl;
+    cout << "Please chose one of the option: " << endl;
+    do
+    {
+        cout << "     (1) Create list" << endl;
+        cout << "     (2) Add an item to a list." << endl;
+        cout << "     (3) Verifie an item from a list" << endl;
+        cout << "     (4) Verifie a listshop of a date" << endl;
+        cout << "     (5) Exit" << endl;
+        cin >> a;
+        switch (a)   //case switches to know which opcion is selected
+        {
+        case 1:
+            makelist();
+            break;
+        case 2:
+            addtolist();
+            break;
+            /*case 3:
+            checkitem();
+            break;
+            case 4:
+            displaylist();
+            break;*/
+            case 5:
+                cout << "...Closing...";
+                return 0;
+            default:
+                cout << "..ERROR..introduce a number from 1 to 5";
+                break;
+        }
+    } while (a != 5);   //if the case is not 5 its going to return an error message
+    return 0;
 }
-
-void Menu::clearScreen()
+void makelist()
 {
-	#ifdef _WIN32
-	system("cls");
-	#else
-	system("clear");
-	#endif
+    string FileName;
+    cout << "Enter a name for the list: ";
+    cin >> FileName;
+    ofstream WriteToFile;
+    WriteToFile.open(FileName);
+    list L;
+    int n;
+    cout << "Enter a name for the items:" << endl;
+    cin >> n;
+    for (int i = 0; i <= n; i++) //cicle for the items
+    {
+        cout << "Enter items and quantity:" << endl;
+        cin >> L.items >> L.quant;
+        WriteToFile << L.items << L.quant;
+    }
+    cout << "Enter the name of the supermarket:" << endl;
+    cin >> L.place;
+    WriteToFile << L.place;
+    cout << "Entrar date:" << endl;
+    cin >> L.date;
+    WriteToFile << L.date;
 }
-
-void Menu::pause()
+void addtolist()
 {
-	#ifdef _WIN32
-	system("pause");
-	#else
-	system("read -n1 -r -p \"Press any key to continue...\" key");
-	#endif
+    cout << "ur lists:" << endl;     // and display the saved lists for the user to choose
+    ifstream readfromfile;
+    if (readfromfile.is_open())
+    {
+        makelist();              //the user adding new item
+    }
+    else
+        cout << "ERROR opening the document" << endl;
 }
-
-
-bool Menu::validate(int option)
-{
-	if(option >= 1 && option <= 4)
-	{
-		return true;
-	}else
-	{
-		cout << "Please pick an option between 1 and 4"<< endl;
-		pause();
-		return false;
-	}
-}
-
-void Menu::routeAction(int option)
-{
-	switch(option)
-	{
-	case 1:
-		listItems();
-		break;
-	case 2:
-		addItems();
-		break;
-	case 3:
-		removeItems();
-		break;
-	case 4:
-		cout << "Thanks for using our software, good bye!" << endl;
-	}
-	pause();
-}
-
-void Menu::listItems()
-{
-	//TODO: Implement this
-	cout << "Implement  Menu::listItems" << endl;
-}
-
-void Menu::addItems()
-{
-	//TODO: Implement this
-	cout << "Implement  Menu::addItems" << endl;
-}
-
-void Menu::removeItems()
-{
-	//TODO: Implement this
-	cout << "Implement  Menu::removeItems" << endl;
-}
-
-void Menu::show()
-{
-	int option;
-	do
-	{
-		clearScreen();
-		cout << "ITLA Shopping List" << endl;
-		cout << "1- List items" << endl;
-		cout << "2- Add Item" << endl;
-		cout << "3- Remove Item" << endl;
-		cout << "4- Exit" << endl;
-		cout << "Select an option => ";
-		cin >> option;
-		if(validate(option))
-		{
-			routeAction(option);
-		}
-	}while(option !=4);
-}
+/*void checkitem()          // function for displaying list from file for the user to chech them which i don't know how to do it
+{}
+void displaylist()          // function displaying list from file which i also don't know how
+{}*/
